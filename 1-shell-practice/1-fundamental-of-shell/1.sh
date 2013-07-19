bfeff67ae530b465048ca3f610a5d4c9e8c9f3cc
root <root@xxx-Lenovo-V360.(none)>
2013-07-19 19:34:42 +0800

    complete task1

---
+++ 1-shell-practice/1-fundamental-of-shell/1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ $# -eq 1 ]
+then 
+	echo "$1$1"
+else
+	echo 'error'
+fi
+
+
---
+++ 1-shell-practice/1-fundamental-of-shell/2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+DATA=$(date +%Y%m%d)
+touch "/home/xxx/Desktop/test/temp_$DATA" 
+echo "$DATA"
+
