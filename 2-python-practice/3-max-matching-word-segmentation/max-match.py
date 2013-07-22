16c6fac90f0de51afb6163f08ee62443b7d8c58c
root <root@xxx-Lenovo-V360.(none)>
2013-07-22 15:16:02 +0800

    fail to load dic

--- 2-python-practice/3-max-matching-word-segmentation/max-match.py
+++ 2-python-practice/3-max-matching-word-segmentation/max-match.py
@@ -4,7 +4,22 @@ import sys
 
 def max_match_segment( line, dic ):
     # write your code here
-
+	result=""
+	maxnum,nodenum=7,3
+	while True:
+		if len(line)<maxnum*nodenum:
+			maxnum=len(line)/nodenum
+		if len(line)==0: break
+		for x in range(maxnum*nodenum,0,-nodenum):
+			words=line[0:x]
+			if x==nodenum:
+				line.replace(words,'',1)
+				result+=words+"/t"
+				break
+			if dic.find(words):
+				line.replace(words,'',1)
+				result+=words+"/t"
+				break
 if __name__=="__main__":
 
     try:
